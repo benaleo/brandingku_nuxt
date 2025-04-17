c950d33317b349fb5914f76d679e430cdd984dde
benaleo <benaleo.bayu.satria@unictive.com>
2025-04-17 13:56:02 +0700

    enhance sidebar with dynamic icons for menu groups and items

--- components/elements/AppSidebar.vue
+++ components/elements/AppSidebar.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-vue-next"
 import {
   Sidebar,
   SidebarContent,
@@ -11,26 +10,35 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible'
-import { ChevronDown } from 'lucide-vue-next'
+import { ChevronDown, Home, LayoutDashboard, Package, Layers } from 'lucide-vue-next'
 import { secretConsoleMenu } from '~/utils/list.menu'
 
-// Menu items.
-const items = secretConsoleMenu
+const iconComponents = {
+  Home,
+  LayoutDashboard,
+  Product: Package,
+  'Produk Kategori': Layers
+}
 </script>
 
 <template>
   <Sidebar class="w-64 border-r">
     <SidebarContent>
       <Collapsible 
-        v-for="group in items" 
+        v-for="group in secretConsoleMenu" 
         :key="group.group_name"
         defaultOpen 
         class="group/collapsible"
       >
         <SidebarGroup>
           <SidebarGroupLabel asChild>
-            <CollapsibleTrigger class="w-full flex items-center">
-              {{ group.group_name }}
+            <CollapsibleTrigger class="w-full flex items-center gap-2">
+              <component 
+                v-if="group.group_icon" 
+                :is="iconComponents[group.group_icon] || iconComponents[group.group_name]" 
+                class="h-4 w-4"
+              />
+              <span>{{ group.group_name }}</span>
               <ChevronDown class="ml-auto h-4 w-4 transition-transform group-data-[state=open]/collapsible:rotate-180" />
             </CollapsibleTrigger>
           </SidebarGroupLabel>
@@ -43,7 +51,11 @@ const items = secretConsoleMenu
                       :to="item.href" 
                       class="flex items-center gap-2 px-4 py-2"
                     >
-                      <component v-if="item.icon" :is="item.icon" class="h-4 w-4" />
+                      <component 
+                        v-if="item.icon" 
+                        :is="iconComponents[item.icon] || iconComponents[item.name]" 
+                        class="h-4 w-4" 
+                      />
                       <span>{{ item.name }}</span>
                     </NuxtLink>
                   </SidebarMenuButton>
