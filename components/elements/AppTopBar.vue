ef32c4c361e8c5b76b8f8d4f9310746fdad651a7
benaleo <benaleo.bayu.satria@unictive.com>
2025-04-17 14:14:22 +0700

    add AppTopBar component and integrate it into console layout

---
+++ components/elements/AppTopBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full h-[40px] bg-slate-200 flex items-center px-4 justify-end">
+    test
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- layouts/console-secret.vue
+++ layouts/console-secret.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import { SidebarProvider } from '@/components/ui/sidebar'
+import {SidebarProvider} from '@/components/ui/sidebar'
 import AppSidebar from '~/components/elements/AppSidebar.vue'
+import AppTopBar from "~/components/elements/AppTopBar.vue";
 </script>
 
 <template>
   <SidebarProvider>
-    <div class="flex min-h-screen">
-      <AppSidebar />
+    <div class="flex w-full min-h-screen">
+      <AppSidebar/>
       <div class="flex-1 overflow-auto bg-background">
-        <slot />
+        <AppTopBar/>
+        <div class="px-8 py-4">
+          <slot/>
+        </div>
       </div>
     </div>
   </SidebarProvider>
