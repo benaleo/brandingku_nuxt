66da9a120ecdf3d9d479b47062a2226c9f333fe8
benaleo <benaleobayu@gmail.com>
2025-04-23 02:25:52 +0700

    init navbar for mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -17,6 +17,20 @@
       </div>
     </div>
   </nav>
+  <nav class="fixed top-5 left-1/2 w-[90vw] transform -translate-x-1/2 z-10 md:hidden block z-20">
+    <div class="overflow-hidden w-full h-[60px] px-2 md:px-4 bg-yellow-50 rounded-md shadow md:sticky top-0 flex justify-between items-center">
+      <div class="flex items-center justify-start">
+        <NuxtLink to="/">
+          <img class="h-[50px]" src="/images/html/logo.png" alt="logo">
+        </NuxtLink>
+  
+      </div>
+      <div>
+        <Button @click="router.push('/product')" variant="primary">Products</Button>
+      </div>
+    </div>
+  </nav>
+
 </template>
 
 <script setup lang="ts">
