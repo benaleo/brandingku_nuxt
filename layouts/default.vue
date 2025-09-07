f7df28042f536c80019d466256ae807801e24a5c
benaleo <benaleo.bayu.satria@unictive.id>
2025-09-07 09:45:25 +0700

    upd : layout all products

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <Navbar />
-  <slot />
+  <main class="bg-green-100 min-h-screen">
+    <slot />
+  </main>
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Navbar from "~/components/Navbar.vue";
+import Footer from "~/components/Footer.vue";
 </script>
--- layouts/page-layout.vue
+++ layouts/page-layout.vue
@@ -1,9 +1,12 @@
 <template>
-  <Navbar/>
-  <slot/>
+<Navbar/>
+  <main class="bg-white min-h-screen">
+    <slot/>
+  </main>
   <Footer class="mt-12"/>
 </template>
 
 <script setup lang="ts">
-import Navbar from "~/components/Navbar.vue";
+import Navbar from '~/components/Navbar.vue';
+
 </script>
