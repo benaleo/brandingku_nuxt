45bf5e663d7370f1aa67bcf7b1b315c85c66cf26
benaleo <benaleo.bayu.satria@unictive.id>
2025-09-07 11:00:30 +0700

    upd : mobile product detail info padding

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -214,7 +214,7 @@ definePageMeta({
         </div>
 
         <!-- Info -->
-        <div class="pr-12">
+        <div class="pr-0 md:pr-12">
           <h1 class="text-2xl md:text-3xl font-bold tracking-tight">
             {{ detail.name }}
           </h1>
