aa2631559ed7bfee24f9bdc1673caba45a42b1b1
benaleo <benaleo.bayu.satria@unictive.id>
2026-03-21 21:52:28 +0700

    add : out service section

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,39 @@
 <script setup>
 // Import shadcn-vue components
-import { Button } from '@/components/ui/button'
-import { Badge } from '@/components/ui/badge'
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import FeaturesServices from "~/components/FeaturesServices.vue";
 
-const pageTitle = 'Brandingku - Best Merchandise Supplier'
+const pageTitle = "Brandingku - Best Merchandise Supplier";
 
 useHead({
   title: pageTitle,
-})
+});
 
 definePageMeta({
-  layout: 'default'
-})
+  layout: "default",
+});
 </script>
 
 <template>
   <div class="min-h-screen bg-white">
-
     <!-- Hero Section -->
-     <Hero/>
+    <Hero />
 
     <section class="app-container">
+      <!-- Our Services -->
+      <FeaturesServices />
+
       <!-- Featured Categories -->
-       <FeaturedCategories/>
+      <FeaturedCategories />
 
       <!-- Featured Products -->
-       <FeaturesProduct/>
-      
+      <FeaturesProduct />
     </section>
 
     <!-- Benefit FAQ -->
 
     <!-- Newsletter -->
     <BannerCTA />
-
   </div>
 </template>
